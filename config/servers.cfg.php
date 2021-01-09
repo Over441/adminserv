6bc413b22725460b391742e260e8ae9a98a370ad
Over441 <dessart.loo@orange.fr>
2021-01-09 21:43:23 +0100

    GO !

---
+++ config/servers.cfg.php
@@ -0,0 +1,18 @@
+<?php
+class ServerConfig {
+    public static $SERVERS = array(
+        /********************* SERVER CONFIGURATION *********************/
+
+        /*
+        'new server name' => array(
+            'address'		=> 'localhost',
+            'port'			=> 5000,
+            'mapsbasepath'	=> '',
+            'matchsettings'	=> 'MatchSettings/',
+            'adminlevel'	=> array('SuperAdmin' => 'all', 'Admin' => 'all', 'User' => 'all'),
+            'ds_pw'         => 'User'
+        ),
+        */
+    );
+}
+?>
